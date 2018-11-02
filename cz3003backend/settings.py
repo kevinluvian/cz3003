b46ddc73bb2e1ec2bdb7508d1f807960da95ef72
Kevin Luvian <kluvian001@e.ntu.edu.sg>
2018-11-03 06:37:00 +0800

    add whitenoise

--- cz3003backend/settings.py
+++ cz3003backend/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/production/static/'
+WHITENOISE_STATIC_PREFIX = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
